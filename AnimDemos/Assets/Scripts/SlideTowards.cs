cb59a757166a3d8020399cd8f8d8ab90960c8279
Gavin <szczesg@ferris.edu>
2021-02-11 18:30:57 -0500

    Week 5 - Added Quaternion Function to AnimMath Class
    - Added Quaternions

--- AnimDemos/Assets/Scripts/SlideTowards.cs
+++ AnimDemos/Assets/Scripts/SlideTowards.cs
@@ -6,8 +6,28 @@ public class SlideTowards : MonoBehaviour {
     
     public Transform target;
 
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+    }
+
     void Update()
     {
+        // This slides the position towards the target:
         transform.position = AnimMath.Slide(transform.position, target.position, .01f);
+
+        // Ease camera rotation to look at the target
+
+        // Get vector to target
+        Vector3 VectorToTarget = target.position - cam.transform.position;
+
+        // Creates the desired rotation
+        Quaternion targetRotation = Quaternion.LookRotation(VectorToTarget, Vector3.up);
+
+        // Set camera to ease towards the target rotation
+        cam.transform.rotation = AnimMath.Slide(cam.transform.rotation, targetRotation, .05f);
+
     }
 }
